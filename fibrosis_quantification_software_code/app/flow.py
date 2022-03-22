abc3f97d2371e6fdc909c191a73cbdd8b9d2cdf7
sergeiissaev <sergei74@my.yorku.ca>
2022-03-22 16:53:14 -0400

    added visualization

---
+++ fibrosis_quantification_software_code/app/flow.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from prefect import Flow
+
+import fibrosis_quantification_software_code.fibrosis_quantification.blob_removal as blob_removal
+import fibrosis_quantification_software_code.fibrosis_quantification.fibrosis_quantification as fibrosis_quantification
+
+
+def create_flow(patch_or_wsi: str, uploaded_file):
+    with Flow("fibrosis-quant-flow") as flow:
+        model = fibrosis_quantification.import_model()
+        (
+            num_samples,
+            im1_preprocess_blocks,
+            img_preprocess_blocks_255,
+            width,
+            height,
+            patchwise_thresholded_tissue_nontissue,
+        ) = fibrosis_quantification.preliminary_preprocessing(uploaded_file, patch_or_wsi)
+        grid2d, threshgenner, thresh_tissue = fibrosis_quantification.apply_gan(
+            num_samples, model, im1_preprocess_blocks, img_preprocess_blocks_255, width, height
+        )
+        clean_thresholded_fibrosis_nonfibrosis, remove = fibrosis_quantification.clean_images(
+            width, height, grid2d, threshgenner, thresh_tissue
+        )
+        patchwise_thresholded_tissue_nontissue = blob_removal.blob_removal(
+            patch_or_wsi, patchwise_thresholded_tissue_nontissue, remove, num_samples, height, width
+        )
+        tissue_final, fibrosis_final = fibrosis_quantification.report_fibrosis(
+            patchwise_thresholded_tissue_nontissue, patch_or_wsi, clean_thresholded_fibrosis_nonfibrosis
+        )
+    return flow
+
+
+if __name__ == "__main__":
+    flow = create_flow(patch_or_wsi="patch", uploaded_file=0)
+    flow.visualize()
--- fibrosis_quantification_software_code/app/main.py
+++ fibrosis_quantification_software_code/app/main.py
@@ -4,10 +4,8 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 import streamlit as st
 from art import tprint
-from prefect import Flow
 
-import fibrosis_quantification_software_code.fibrosis_quantification.blob_removal as blob_removal
-import fibrosis_quantification_software_code.fibrosis_quantification.fibrosis_quantification as fibrosis_quantification
+from fibrosis_quantification_software_code.app.flow import create_flow
 from fibrosis_quantification_software_code.app.footer import footer
 
 
@@ -27,36 +25,8 @@ def main():
         if clicked:
             st.markdown("<h5 style='text-align: center;'>Original image</h1>", unsafe_allow_html=True)
             st.image(uploaded_file)
-            with Flow("fibrosis-quant-flow") as flow:
-                model = fibrosis_quantification.import_model()
-                (
-                    num_samples,
-                    im1_preprocess_blocks,
-                    img_preprocess_blocks_255,
-                    width,
-                    height,
-                    patchwise_thresholded_tissue_nontissue,
-                ) = fibrosis_quantification.preliminary_preprocessing(uploaded_file, radio)
-                grid2d, threshgenner, thresh_tissue = fibrosis_quantification.apply_gan(
-                    num_samples, model, im1_preprocess_blocks, img_preprocess_blocks_255, width, height
-                )
-                clean_thresholded_fibrosis_nonfibrosis, remove = fibrosis_quantification.clean_images(
-                    width, height, grid2d, threshgenner, thresh_tissue
-                )
-                patchwise_thresholded_tissue_nontissue = blob_removal.blob_removal(
-                    radio, patchwise_thresholded_tissue_nontissue, remove, num_samples, height, width
-                )
-                tissue_final, fibrosis_final = fibrosis_quantification.report_fibrosis(
-                    patchwise_thresholded_tissue_nontissue, radio, clean_thresholded_fibrosis_nonfibrosis
-                )
-
+            flow = create_flow(radio=radio, uploaded_file=uploaded_file)
             state_1 = flow.run()
-            # print(f"state 1 is {state_1}")
-            # for task in flow.get_tasks():
-            #     if task.name == "preliminary preprocessing":
-            #         processing_task = task
-            # task_1_source_image, task_1_result_image = state_1.result[processing_task].result
-
             if state_1.message == "All reference tasks succeeded.":
                 tprint("Success")
 
